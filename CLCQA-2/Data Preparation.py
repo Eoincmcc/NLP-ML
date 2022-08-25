26ccde8dc866aafb6acf4a91acf2ba39c8205f95
Eoincmcc <71796225+Eoincmcc@users.noreply.github.com>
2022-08-25 17:47:04 +0900

    CLCQA preparation
    New files to begin working on

---
+++ CLCQA-2/Data Preparation.py
@@ -0,0 +1,15 @@
+# Data Preparation
+
+
+# Importing the libraries
+import numpy as np
+import argparse
+import json
+import os
+import sys
+
+
+def preprocess(args):
+    
+
+def get_tokenizer(name):
